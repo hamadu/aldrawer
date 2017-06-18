5a8d89039c602383856c817bc4ba6a5c4b167625
hamadu <dercll1020@gmail.com>
2017-06-18 23:09:35 +0900

    covered many tests

--- test/unit/model/circleWithText.js
+++ test/unit/model/circleWithText.js
@@ -22,3 +22,17 @@ describe('get/set value', function () {
     assert.equal(circleText.text, 'NEWVALUE')
   })
 })
+
+describe('get/set styles', function () {
+  const circleText = new CircleWithText(25, 80, 30, 'VALUE')
+
+  it('set styles', function () {
+    circleText.setShapeStyles({ 'fill': '#0f0', 'stroke-width': '4px' })
+    assert.equal(circleText.getShapeStyle().fill, '#0f0')
+  })
+
+  it('set single style', function () {
+    circleText.setShapeStyle('hoge', 'moge')
+    assert.equal(circleText.getShapeStyle().hoge, 'moge')
+  })
+})
